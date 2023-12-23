f887cfdd18b8bab49001bd96cfb9649c0ccdab81
sophearyrin1 <srin@miu.edu>
2023-12-23 04:50:24 -0600

    conditional Rendering

--- yidioapp/src/components/ListGroup.tsx
+++ yidioapp/src/components/ListGroup.tsx
@@ -1,9 +1,14 @@
 function ListGroup() {
-  const items = ["New York", "San Fransico", "Denver"];
+  let items = ["New York", "San Fransico", "Denver"];
+  items = [];
+  const getMessage = () => {
+    return items.length == 0 ? <h1>No Item in list</h1> : null;
+  };
   return (
     <>
       <h1>List Group</h1>
       <ul className="list-group">
+        {items.length == 0 && <p>List is Empty</p>}
         {items.map((item) => (
           <li key={item} className="list-group-item">
             {item}
