c9eedf96dabbfd9996b67034b8b238dd7051596e
sophearyrin1 <srin@miu.edu>
2023-12-23 14:06:43 -0600

    Handle event

--- yidioapp/src/components/ListGroup.tsx
+++ yidioapp/src/components/ListGroup.tsx
@@ -1,16 +1,21 @@
+import { MouseEvent } from "react";
+
 function ListGroup() {
   let items = ["New York", "San Fransico", "Denver"];
-  items = [];
   const getMessage = () => {
     return items.length == 0 ? <h1>No Item in list</h1> : null;
+  };
+  //Event Handler
+  const handleClick = (event: MouseEvent) => {
+    console.log(event);
   };
   return (
     <>
       <h1>List Group</h1>
       <ul className="list-group">
         {items.length == 0 && <p>List is Empty</p>}
-        {items.map((item) => (
-          <li key={item} className="list-group-item">
+        {items.map((item, index) => (
+          <li key={item} className="list-group-item" onClick={handleClick}>
             {item}
           </li>
         ))}
