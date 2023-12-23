a82e54a49ae4939ee33aac11408c5c2b57c5b23b
sophearyrin1 <srin@miu.edu>
2023-12-23 14:59:19 -0600

    using state

--- yidioapp/src/components/ListGroup.tsx
+++ yidioapp/src/components/ListGroup.tsx
@@ -1,21 +1,26 @@
-import { MouseEvent } from "react";
+import { useState } from "react";
 
 function ListGroup() {
   let items = ["New York", "San Fransico", "Denver"];
-  const getMessage = () => {
-    return items.length == 0 ? <h1>No Item in list</h1> : null;
-  };
-  //Event Handler
-  const handleClick = (event: MouseEvent) => {
-    console.log(event);
-  };
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
   return (
     <>
       <h1>List Group</h1>
       <ul className="list-group">
         {items.length == 0 && <p>List is Empty</p>}
         {items.map((item, index) => (
-          <li key={item} className="list-group-item" onClick={handleClick}>
+          <li
+            key={item}
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            onClick={() => {
+              setSelectedIndex(index);
+            }}
+          >
             {item}
           </li>
         ))}
