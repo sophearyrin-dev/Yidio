c2d7a9ef21b9d8e3286356605db62b2f91f96fa0
sophearyrin1 <srin@miu.edu>
2023-12-24 01:30:45 -0600

    passing function via prop

--- yidioapp/src/App.tsx
+++ yidioapp/src/App.tsx
@@ -2,9 +2,23 @@ import ListGroup from "./components/ListGroup";
 
 function App() {
   let items = ["New York", "San Fransico", "Denver"];
+  let colorItems = ["Green", "Yellow", "Blue"];
+
+  const handleSeletedItem = (item: string) => {
+    console.log(item);
+  };
   return (
     <div>
-      <ListGroup items={items} heading="List of Cities" />
+      <ListGroup
+        items={items}
+        heading="List of Cities"
+        onSelectedItem={handleSeletedItem}
+      />
+      <ListGroup
+        items={colorItems}
+        heading="List of Color"
+        onSelectedItem={handleSeletedItem}
+      />
     </div>
   );
 }
--- yidioapp/src/components/ListGroup.tsx
+++ yidioapp/src/components/ListGroup.tsx
@@ -1,16 +1,31 @@
+import { useState } from "react";
+
 interface ListGroupProp {
   items: string[];
   heading: string;
+  onSelectedItem: (item: string) => void;
 }
 
-function ListGroup({ items, heading }: ListGroupProp) {
+function ListGroup({ items, heading, onSelectedItem }: ListGroupProp) {
+  const [selectedIndex, setSelectedIndex] = useState(-1);
   return (
     <>
       <h1>{heading}</h1>
       <ul className="list-group">
         {items.length == 0 && <p>List is Empty</p>}
         {items.map((item, index) => (
-          <li key={item} className="list-group-item">
+          <li
+            key={item}
+            className={
+              selectedIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            onClick={() => {
+              setSelectedIndex(index);
+              onSelectedItem(item);
+            }}
+          >
             {item}
           </li>
         ))}
