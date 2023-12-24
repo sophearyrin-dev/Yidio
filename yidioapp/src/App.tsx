3ddad12a31b86b77badf8294013b7414a89a9772
sophearyrin1 <srin@miu.edu>
2023-12-24 02:04:52 -0600

    passing children

--- yidioapp/src/App.tsx
+++ yidioapp/src/App.tsx
@@ -1,24 +1,11 @@
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 
 function App() {
-  let items = ["New York", "San Fransico", "Denver"];
-  let colorItems = ["Green", "Yellow", "Blue"];
-
-  const handleSeletedItem = (item: string) => {
-    console.log(item);
-  };
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading="List of Cities"
-        onSelectedItem={handleSeletedItem}
-      />
-      <ListGroup
-        items={colorItems}
-        heading="List of Color"
-        onSelectedItem={handleSeletedItem}
-      />
+      <Alert>
+        <h1>Hello world</h1>
+      </Alert>
     </div>
   );
 }
